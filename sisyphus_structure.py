f121a7842ce9555c4159ac1f7bc2696611eb9489
Daniel Florez <danielflorezanalista@gmail.com>
2022-06-08 11:01:24 -0500

    Estructura de arranque y números aleatorios sin proteger, almacenados y mostrados

---
+++ sisyphus_structure.py
@@ -0,0 +1,26 @@
+import random
+
+
+def show(random_numbers):
+    for number in random_numbers:
+        print(number)
+
+def random_generator(limit):
+    random_numbers=[]
+    for i in range(limit):
+        number=random.randint(1,10)
+        random_numbers.append(number)
+    return random_numbers
+
+
+def start():
+    limit=int(input("Ingrese el límite de números aleatorios: "))
+    random_numbers=random_generator(limit)
+    show(random_numbers)
+
+def run():
+    start()
+
+
+if __name__=='__main__':
+    run()
